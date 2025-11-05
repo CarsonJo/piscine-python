b744e82db598434a67cb7ca679148b701c7ee556
Carson JOZEFZOON <cjozefzo@f4r2s5.clusters.42paris.fr>
2025-11-05 18:28:31 +0100

    ex05

---
+++ python0/ex05/building.py
@@ -0,0 +1,42 @@
+import sys
+
+
+def start() -> str:
+    sys.tracebacklimit = 0
+    if len(sys.argv) > 2:
+        assert False, "more than one argument is provided"
+    elif len(sys.argv) < 2:
+        print("what is the text to count?")
+        ret: str = sys.stdin.read()
+        return ret
+    return sys.argv[1]
+
+
+def main():
+    upper: int = 0
+    lower: int = 0
+    punctuation: int = 0
+    space: int = 0
+    digits: int = 0
+    my_str: str = start()
+    for i in my_str:
+        if i.isupper():
+            upper += 1
+        elif i.islower():
+            lower += 1
+        elif i.isspace() or i == '\n':
+            space += 1
+        elif i.isdigit():
+            digits += 1
+        elif i.isprintable():
+            punctuation += 1
+    print("The text contain ", end='')
+    print(upper + lower + punctuation + space + digits, "character :")
+    print(upper, "upper letters")
+    print(lower, "lower letters")
+    print(punctuation, "punctuation marks")
+    print(space, "spaces")
+    print(digits, "digits")
+
+
+main()
